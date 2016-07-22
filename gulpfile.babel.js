63c1007e6c69bc3cc87d78806320b9bcfe563af5
Wan Bao <ylxbsoftware@163.com>
2016-07-22 21:10:21 +0800

    简单项目骨架

---
+++ gulpfile.babel.js
@@ -0,0 +1,34 @@
+'use strict';
+import gulp from 'gulp';
+import connect from 'gulp-connect';
+import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer from 'gulp-autoprefixer';
+
+gulp.task('server',() => {
+  connect.server({
+    livereload: true,
+    port:80
+  });
+});
+
+gulp.task('html', () => {
+  gulp.src('./app/*.html')
+  .pipe(connect.reload());
+});
+
+gulp.task('sass',() => {
+  gulp.src('./scss/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./css'))
+  .pipe(connect.reload());
+});
+
+gulp.task('watch',() => {
+  gulp.watch(['./app/*.html'],['html']);
+  // gulp.watch(['./scss/*.scss'],['sass']);
+});
+
+gulp.task('default',['server','watch']);
