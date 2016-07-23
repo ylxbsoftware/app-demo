5ec62c264af43a8f0ac705657c34adc073dddb9e
Wan Bao <ylxbsoftware@163.com>
2016-07-23 09:45:03 +0800

    add sass file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,33 +3,37 @@ import gulp from 'gulp';
 import connect from 'gulp-connect';
 import sourcemaps from 'gulp-sourcemaps';
 import autoprefixer from 'gulp-autoprefixer';
-import sass from 'gulp-sass';
+import sass from 'gulp-ruby-sass';
 
-gulp.task('server',() => {
+gulp.task('server', () => {
   connect.server({
     livereload: true,
-    port:80
+    port: 80
   });
 });
 
 gulp.task('html', () => {
   gulp.src('./app/*.html')
-  .pipe(connect.reload());
+    .pipe(connect.reload());
 });
 
-gulp.task('sass',() => {
-  gulp.src('./scss/*.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(autoprefixer())
+gulp.task('sass', () => {
+  sass('./scss/*.scss', {
+      sourcemap: true
+    }).on('error', sass.logError)
+    .pipe(autoprefixer())
     .pipe(sourcemaps.write())
-  .pipe(gulp.dest('./css'))
-  .pipe(connect.reload());
+    .pipe(sourcemaps.write('maps', {
+      includeContent: false,
+      sourceRoot: 'source'
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(connect.reload());
 });
 
-gulp.task('watch',() => {
-  gulp.watch(['./app/*.html'],['html']);
-  gulp.watch(['./scss/*.scss'],['sass']);
+gulp.task('watch', () => {
+  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./scss/*.scss'], ['sass']);
 });
 
-gulp.task('default',['server','watch']);
+gulp.task('default', ['server', 'watch']);
