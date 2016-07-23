337cbbe0b6a495417831c327c682e2a89a9935ed
Wan Bao <ylxbsoftware@163.com>
2016-07-23 17:03:19 +0800

    add class demo

---
+++ js/form.js
@@ -0,0 +1,16 @@
+class Form {
+  constructor(ele) {
+    this.ele = ele;
+  }
+
+  _getFields() {
+    return document.getElementById(this.ele);
+  }
+
+  setFields(content) {
+    this._getFields().innerHTML = content;
+  }
+}
+
+let form = new Form('content');
+form.setFields('通过class实现赋值内容');
